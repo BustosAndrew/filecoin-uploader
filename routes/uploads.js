ad388925f88887eac7268fef3b37cca88f0ce96b
Andrew Bustos <infernious@live.com>
2022-09-20 22:34:03 -0700

    Added try catch

--- routes/uploads.js
+++ routes/uploads.js
@@ -5,9 +5,13 @@ const { Web3Storage } = require("web3.storage");
 router.post("/upload", async (req, res) => {
 	const files = req.body.files;
 	const token = process.env.TOKEN;
-	const storage = new Web3Storage({ token });
-	const cid = await storage.put(files);
-	res.send(cid + "/" + files.name);
+	try {
+		const storage = new Web3Storage({ token });
+		const cid = await storage.put(files);
+		res.send(cid + "/" + files.name);
+	} catch (err) {
+		res.send(err);
+	}
 });
 
 module.exports = router;
