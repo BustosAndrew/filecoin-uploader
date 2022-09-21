2d9bc89be5b5a71a7637391a35872970570bf6e1
Andrew Bustos <infernious@live.com>
2022-09-21 00:20:05 -0700

    Putting all code in try catch

--- routes/uploads.js
+++ routes/uploads.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const { Web3Storage } = require("web3.storage");
 
 router.post("/upload", async (req, res) => {
-	const files = [];
-	files.push(req.body.files);
-	const token = process.env.TOKEN;
 	try {
+		const files = [];
+		files.push(req.body.files);
+		const token = process.env.TOKEN;
 		const storage = new Web3Storage({ token });
 		const cid = await storage.put(files);
-		res.send(cid + " test");
+		res.send(cid);
 	} catch (err) {
-		res.send(err);
+		res.send(err + "erroring");
 	}
 });
 
