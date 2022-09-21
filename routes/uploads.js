791395421c68c7c1c8661cc1a9073680795874c2
Andrew Bustos <infernious@live.com>
2022-09-20 22:22:54 -0700

    Removed path var and renamed route to upload

--- routes/uploads.js
+++ routes/uploads.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Web3Storage = require("web3.storage");
 
-router.post("/uploads", async (req, res) => {
+router.post("/upload", async (req, res) => {
 	const files = req.body.files;
 	const token = process.env.TOKEN;
 	const storage = new Web3Storage({ token });
