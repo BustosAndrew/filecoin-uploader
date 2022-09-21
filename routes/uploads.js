32d4db4fb0593dafec4c0129c892bf44e450ce83
Andrew Bustos <infernious@live.com>
2022-09-21 01:45:12 -0700

    Converts file object into fileStream

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,15 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const { Duplex } = require("stream"); // Native Node Module
 const { Web3Storage } = require("web3.storage");
+
+function bufferToStream(myBuffer) {
+	let tmp = new Duplex();
+	tmp.push(myBuffer);
+	tmp.push(null);
+	return tmp;
+}
 
 router.post("/upload", async (req, res) => {
 	try {
-		const files = [];
-		files.push(req.body.files);
-		const token = process.env.TOKEN;
-		const storage = new Web3Storage({ token });
-		const cid = await storage.put(files);
-		res.send(cid);
+		if (!req.files) {
+			res.send({
+				status: false,
+				message: "No file uploaded",
+			});
+		} else {
+			const files = [];
+			const fileStream = bufferToStream(req.files.file.data);
+			files.push({ name: req.files.file.name, stream: () => fileStream }); // has to be passed in this way :)
+
+			const token = process.env.TOKEN;
+			const storage = new Web3Storage({ token });
+			const cid = await storage.put(files);
+			const fileUrl = encodeURI(
+				`https://${cid}.ipfs.w3s.link/${req.files.file.name}`
+			);
+
+			res.send({ fileUrl });
+		}
 	} catch (err) {
 		res.send(err);
 	}
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
 
 // express config
 const app = express();
 const port = process.env.PORT || 5000;
 
+// enable files upload
+app.use(
+	fileUpload({
+		createParentPath: true,
+	})
+);
+
 // express middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(express.static(path.join(__dirname, "public")));
 
 // express routers
