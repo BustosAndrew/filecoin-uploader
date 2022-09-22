52a9df25160bb6500d79c107cddc9bc40f3ed4b0
Andrew Bustos <infernious@live.com>
2022-09-22 16:39:26 -0700

    Update uploads.js

--- routes/uploads.js
+++ routes/uploads.js
@@ -12,25 +12,18 @@ function bufferToStream(myBuffer) {
 
 router.post("/upload", async (req, res) => {
 	try {
-		if (!req.files) {
-			res.send({
-				status: false,
-				message: "No file uploaded",
-			});
-		} else {
-			const files = [];
-			const fileStream = bufferToStream(req.files.file.data);
-			files.push({ name: req.files.file.name, stream: () => fileStream }); // has to be passed in this way :)
+		const files = [];
+		const fileStream = bufferToStream(req.files.file.data);
+		files.push({ name: req.files.file.name, stream: () => fileStream }); // has to be passed in this way :)
 
-			const token = process.env.TOKEN;
-			const storage = new Web3Storage({ token });
-			const cid = await storage.put(files);
-			const fileUrl = encodeURI(
-				`https://${cid}.ipfs.w3s.link/${req.files.file.name}`
-			);
+		const token = process.env.TOKEN;
+		const storage = new Web3Storage({ token });
+		const cid = await storage.put(files);
+		const fileUrl = encodeURI(
+			`https://${cid}.ipfs.w3s.link/${req.files.file.name}`
+		);
 
-			res.send({ fileUrl });
-		}
+		res.send({ fileUrl });
 	} catch (err) {
 		res.send(err);
 	}
